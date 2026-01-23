0f0de045b2bf08ab74bf5744e8acbe791b11ab9a
David42024 <daln486279513@gmail.com>
2026-01-23 10:46:52 -0500

    proxi

--- app/Http/Middleware/TrustProxies.php
+++
@@ -1,7 +0,0 @@
-<php 
-
-use Illuminate\Http\Request;
-
-protected $proxies = '*';
-
-protected $headers = Request::HEADER_X_FORWARDED_ALL;
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_ALL
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
